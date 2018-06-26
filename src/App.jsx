d89882ca5f49644a9f39a040a8fcd8729f96a6f4
Lex <webdesignbylex@gmail.com>
2018-06-25 22:21:01 -0400

    removed react-router and google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 //react imports
 import * as React from 'react';
 
-// react-router
-import { MemoryRouter } from 'react-router-dom';
-
-//google analytics
-import Analytics from 'react-router-ga';
-
 // views
 import Landing from './views/Landing.jsx';
 import Coverage from './views/Coverage.jsx';
@@ -20,15 +14,11 @@ import './style/css/app.min.css';
 export default class App extends React.Component {
   render() {
     return (
-      <MemoryRouter>
-        <Analytics id="" debug>
-          <React.Fragment>
-            <Landing />
-            <Coverage />
-            <Footer />
-          </React.Fragment>
-        </Analytics>
-      </MemoryRouter>
+      <React.Fragment>
+        <Landing />
+        <Coverage />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
